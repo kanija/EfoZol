b94990923fc59ced887d829ee81dafc0587f3ad6
mberaz <mbearz@gmail.com>
2015-08-03 19:08:57 -0700

    fixed error page/handling

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ var router       = express.Router();
 var routes       = require('./routes.js').routes;
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -36,27 +37,35 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Resurce Not Found');
   err.status = 404;
   next(err);
 });
 
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
+    res.status(err.status || 500);  
+    res.json({
+  	     "Error": err.message,
+      "status":err.status     
+  });
+   /* res.render('error', {
       message: err.message,
       error: err
-    });
+    });*/
   });
 }
 
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+    res.json({
+  	     "Error": err.message,
+      "status":err.status     
   });
+   /* res.render('error', {
+      message: err.message,
+      error: err
+    });*/
 });
 
 
